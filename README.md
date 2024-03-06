e586147846d5970c2d43a3d22677d0d67027d69c
kimkhoathao <kimkhoathao@gmail.com>
2024-03-06 09:25:32 +0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Phepcong
+
+Một thư viện giúp bạn tính tổng của 2 số bất kỳ.
+Đơn giản, dễ dùng, cực kỳ nhẹ
---
+++ phepcong.js
@@ -0,0 +1,5 @@
+/* Dependencies */
+const areObjects = require('are-objects');
+const areArrays = require('are-arrays');
+const sum = (a, b) => a + b
+exports.sum = sum
